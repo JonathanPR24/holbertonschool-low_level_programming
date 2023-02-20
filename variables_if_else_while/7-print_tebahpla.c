98a8654da858d8f45efe0e5dd2005374cbfb1ba6
jonathanPR24 <jonathanperez604@gmail.com>
2023-02-20 21:23:54 +0000

    prints tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints tebahpla
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar (n);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
