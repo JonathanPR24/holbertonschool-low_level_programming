103623a6cde2095502cd70bd861829f961c34990
JonathanPR24 <jonathanperez604@gmail.com>
2023-02-23 23:09:08 +0000

    prints narutal number

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,31 @@
-#include
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @n: starting number
+ *
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+
+	printf("%d\n", 98);
+}
