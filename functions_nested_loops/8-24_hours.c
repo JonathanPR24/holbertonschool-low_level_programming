1be67aed9fd002e86a747b1b1b492367201b4179
JonathanPR24 <jonathanperez604@gmail.com>
2023-02-23 17:44:01 +0000

    prints every minute

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ */
+
+void jack_bauer(void)
+{
+	int hour, minutes;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
