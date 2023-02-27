da7dc135b750008aa31b143dacef4f5ed071971b
JonathanPR24 <jonathanperez604@gmail.com>
2023-02-27 17:59:38 +0000

    function that takes a pointer

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - method that modifies the value of an int pointer that is sent as an argument to 98
+ * reset_to_98 - method that modifies the value of an int pointer that
+ * is sent as an argument to 98
  *
  * @n: this is the entry
  */
