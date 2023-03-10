9fc2e12f2f3d084b1b1e29980db51a184fda0a49
JonathanPR24 <jonathanperez604@gmail.com>
2023-03-10 17:43:30 +0000

    prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints its name
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: Always 0 (success)
+ */
+
+#include <stdio.h>
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
