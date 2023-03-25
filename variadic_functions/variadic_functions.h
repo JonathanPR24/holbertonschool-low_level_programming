b516bdced6bdbbb4504f090ee9d5aafc44ca8222
JonathanPR24 <jonathanperez604@gmail.com>
2023-03-25 00:29:02 +0000

    prototype

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,3 +1,8 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -5,3 +10,5 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
