7e4ddf11097a41bcaff727c4755827a3ab90e8bc
JonathanPR24 <jonathanperez604@gmail.com>
2023-04-12 01:55:55 +0000

    copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,72 +2,65 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include "main.h"
 
-#define BUFSIZE 1024
-
+/**
+ * main - copies the content of a file to another file.
+ * @argc: number of arguments passed.
+ * @argv: array of arguments.
+ * Return: 0 on success, or appropriate error code on failure.
+ */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, ret;
-	char buf[BUFSIZE];
-	ssize_t bytes_read, bytes_written;
-	struct stat st;
+	int from_fd, to_fd, rd_ret, wr_ret;
+	char buffer[1024];
+	mode_t file_perms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	from_fd = open(argv[1], O_RDONLY);
+	if (from_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	ret = fstat(fd_from, &st);
-	if (ret == -1)
+	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perms);
+	if (to_fd == -1)
 	{
-		perror("fstat");
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, st.st_mode & 0777);
-	if (fd_to == -1)
+	while ((rd_ret = read(from_fd, buffer, 1024)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		exit(99);
-	}
-
-	while ((bytes_read = read(fd_from, buf, BUFSIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buf, bytes_read);
-		if (bytes_written == -1)
+		wr_ret = write(to_fd, buffer, rd_ret);
+		if (wr_ret == -1)
 		{
-			perror("write");
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 
-	if (bytes_read == -1)
+	if (rd_ret == -1)
 	{
-		perror("read");
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	ret = close(fd_from);
-	if (ret == -1)
+	if (close(from_fd) == -1)
 	{
-		perror("close");
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
 		exit(100);
 	}
 
-	ret = close(fd_to);
-	if (ret == -1)
+	if (close(to_fd) == -1)
 	{
-		perror("close");
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
 		exit(100);
 	}
 
